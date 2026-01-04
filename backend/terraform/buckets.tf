bfa41260531b14fbbaf1af525c4cc4461219983e
Will <willgraham12345@hotmail.com>
2026-01-04 08:03:15 +0000

    test cloud function

--- backend/terraform/buckets.tf
+++ backend/terraform/buckets.tf
@@ -1,7 +1,14 @@
-resource "google_storage_bucket" "test_bucket" {
+resource "google_storage_bucket" "function_source_code_bucket" {
   name     = "test_bucket_fh285"
   location = "europe-west2"
   project  = var.project_id
 
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket_object" "hello_world_source_code" {
+  name   = "hello_world.zip"
+  bucket = google_storage_bucket.function_source_code_bucket.name
+  source = "../cloud_functions/hello_world.zip"
+}
+
---
+++ backend/terraform/cloud_functions.tf
@@ -0,0 +1,21 @@
+resource "google_cloudfunctions_function" "function" {
+  name        = "hello_world"
+  description = "Test function"
+  runtime     = "python39"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.function_source_code_bucket.name
+  source_archive_object = google_storage_bucket_object.hello_world_source_code.name
+  trigger_http          = true
+  entry_point           = "main"
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
