613cb5e7e6f0b05bad841410a1848805d52d720c
Will <willgraham12345@hotmail.com>
2026-01-04 08:59:02 +0000

    test cloud function

--- backend/terraform/cloud_functions.tf
+++ backend/terraform/cloud_functions.tf
@@ -16,7 +16,7 @@ resource "google_cloudfunctions2_function" "hello_world" {
 
   service_config {
     max_instance_count = 1
-    available_memory   = "128M"
+    available_memory   = "128Mi"
     timeout_seconds    = 15
   }
 }
