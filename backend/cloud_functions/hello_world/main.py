5b1888eb96476d0efa20b1137781c6f2746658d7
Will <willgraham12345@hotmail.com>
2026-01-06 08:06:21 +0000

    frontend - backend comms

--- backend/cloud_functions/hello_world/main.py
+++ backend/cloud_functions/hello_world/main.py
@@ -5,11 +5,26 @@ from flask import Request
 class HelloRequest(BaseModel):
     name: str
 
+def cors_response():
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Max-Age": "3600"
+    }
+    return ("", 204, headers)
+
 @functions_framework.http
 def hello_world(request: Request):
+
+    if request.method == "OPTIONS":
+        return cors_response()
+
+    headers = {"Access-Control-Allow-Origin": "*"}
+
     try:
         req = HelloRequest.model_validate(request.args)
     except ValidationError as e:
-        return f"Invalid request format: {e}!"
+        return (f"Invalid request format: {e}!", 400, headers)
 
-    return f"Hello {req.name}!"    
+    return (f"Hello {req.name}!", 200, headers)    
