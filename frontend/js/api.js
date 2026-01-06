c060d4dcf42d25c195aa8c33125bd5da131ace37
Will <willgraham12345@hotmail.com>
2026-01-06 08:30:57 +0000

    frontend cleaner

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,7 @@
+const urlRoot = 'https://europe-west2-potent-thought-483207-v4.cloudfunctions.net'
+
 export async function getHello(name) {
-    const response = await fetch(`${url}?name=${encodeURIComponent(name)}`, {
+    const response = await fetch(`${urlRoot}/hello-world?name=${encodeURIComponent(name)}`, {
         method: 'GET'
     });
     return response.text();
