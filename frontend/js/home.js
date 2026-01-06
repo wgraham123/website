ec19bcf91036298a25478790f3ad1fcf9f626b95
Will <willgraham12345@hotmail.com>
2026-01-06 08:28:45 +0000

    frontend cleaner

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,7 +1,7 @@
 import { getHello } from "./api.js";
 
-document.getElementById("helloBtn").onClick = async () => {
+document.getElementById("helloBtn").addEventListener("click", async () => {
     const name = document.getElementById("nameInput").value;
     const result = await getHello(name);
     document.getElementById("result").innerText = result;
-};
+});
