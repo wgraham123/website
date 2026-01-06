9dbeda439c8b6fbc51284cf0224d755e528c25c9
Will <willgraham12345@hotmail.com>
2026-01-06 07:44:50 +0000

    cloud function with query parameters

--- backend/cloud_functions/hello_world/main.py
+++ backend/cloud_functions/hello_world/main.py
@@ -1,7 +1,15 @@
 import functions_framework
+from pydantic import BaseModel, ValidationError
 from flask import Request
+
+class HelloRequest(BaseModel):
+    name: str
 
 @functions_framework.http
 def hello_world(request: Request):
-    name = request.args
-    return f"Hello {name}!"    
+    try:
+        req = HelloRequest.model_validate(request.args)
+    except ValidationError as e:
+        return f"Invalid request format: {e}!"
+
+    return f"Hello {req.name}!"    
